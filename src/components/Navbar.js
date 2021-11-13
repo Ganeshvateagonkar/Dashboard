f56ba0581997b0dc8b61f3d87107a00a6834c03e
ganesh vategaonkar <ganeshvat17@gmail.com>
2021-11-12 22:45:39 -0800

    hamburger modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import "../App.css"
 
-const Navbar = () => {
+const Navbar = (props) => {
+
 
     return (
         <div className="navbar">
             <a href='/'>
-                <h3 >Home</h3>
+
+                <h3 >
+                    Home</h3>
             </a>
         </div>
     )
